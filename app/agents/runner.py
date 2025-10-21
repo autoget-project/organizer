42689c82d93e1b06b7a6bdb99fbac704d8a5f690
charleshuang3 <charleshuang233@gmail.com>
2025-10-20 22:29:56 -0400

    feat: runner call tv_series_mover_agent

--- app/agents/runner.py
+++ app/agents/runner.py
@@ -1,5 +1,6 @@
 from .categorizer import agent as categorizer_agent
 from .movie_mover import agent as movie_mover_agent
+from .tv_series_mover import agent as tv_series_mover_agent
 from .models import Category, PlanRequest, PlanResponse, simple_move_categories
 from .ai import metadataMcp
 from utils.utils import simple_move_plan
@@ -14,6 +15,10 @@ async def create_plan(req: PlanRequest) -> PlanResponse:
   if cat in simple_move_categories:
     return simple_move_plan(cat, req.files)
 
-  if cat == Category.movie.name or cat == Category.anim_movie.name:
+  if cat == Category.movie or cat == Category.anim_movie:
     movie_mover = movie_mover_agent(mcp, categorizer_res.output)
     return await movie_mover.run(req)
+
+  if cat == Category.tv_series or cat == Category.anim_tv_series:
+    tv_series_mover = tv_series_mover_agent(mcp, categorizer_res.output)
+    return await tv_series_mover.run(req)
